f68e9eab09feeaf7e5ee7330e0f738853849908d
unknown <spncrkm@gmail.com>
2024-05-09 17:55:20 -0400

    Forgot to do put problem #3

--- hwday3week2.py
+++ hwday3week2.py
@@ -71,3 +71,18 @@ while True:
         print("Invalid input. Please enter 'yes', 'no' or 'display'.") 
 
 
+# 3. Mastering Tuple Packing and Unpacking in Python
+# Objective: The goal of this assignment is to deepen your understanding of tuple packing and unpacking in Python. You will apply these techniques in various practical scenarios, enhancing your ability to work with flexible data 
+# structures and improve data handling efficiency.
+
+# Task 1: Customer Order Processing Refine your skills in tuple unpacking by managing customer orders.
+
+# Problem Statement: You are given a list of tuples, each representing a customer's order. Each tuple contains the customer's name, the product ordered, and the quantity. Your task is to unpack each tuple and print the order details in a user-friendly format.
+
+orders = [
+    ("Alice", "Laptop", 1),
+    ("Bob", "Camera", 2),
+]
+
+for name, item, quantity in orders:
+    print(f"{name} purchased {item} quantity of {quantity}.")
